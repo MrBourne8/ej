dfd67c24989292e7c27ed1d871679f1c33e195d2
MrBourne8 <35061836+MrBourne8@users.noreply.github.com>
2025-03-22 10:52:11 +0700

    133

--- app/includes/title.php
+++ app/includes/title.php
@@ -1,6 +1,6 @@
 <div class="header__wrap">
     <div class="logo">
-        <i class="fas fa-graduation-cap logo__icon"></i>
+        <i class="fa fa-book logo__icon" aria-hidden="true"></i>
         <h3 class="header__title" style="font-weight: bold;">ЭЛЕКТРОННЫЙ ЖУРНАЛ <span>beta</span> </h3>
         <p class="header__subtitle">ГАПОУ НСО "Карасукский педагогический колледж"</p>
     </div>
