56ec198d17d5992d70ca0162b5dbf425fbf3eb5e
MrBourne8 <antohas8@rambler.ru>
2025-12-31 10:03:37 +0700

    123

--- app/db.php
+++ app/db.php
@@ -3,7 +3,9 @@
 require 'libs/rb.php';
 R::setup(
     'mysql:host=localhost;dbname=u1392839_karasuk_spo',
-'u1392839_default', 'BAhDp2NpF2FM5hd1' ); //for both mysql or mariaDB
+    'u1392839_default',
+    'BAhDp2NpF2FM5hd1'
+); //for both mysql or mariaDB
 
 session_start();
 
